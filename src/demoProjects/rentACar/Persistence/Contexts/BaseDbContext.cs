533996a019cf80fca1acff99458924361fc61c07
NURULLAH CİVELEK <nu_civelek@hotmail.com>
2022-09-22 16:01:41 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Model> Models { get; set; }
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            //    base.OnConfiguring(
+            //        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("SomeConnectionString")));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>(b =>
+            {
+                b.ToTable("Brands").HasKey(k => k.Id);
+                b.Property(p => p.Id).HasColumnName("Id");
+                b.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            modelBuilder.Entity<Model>(m =>
+            {
+                m.ToTable("Models").HasKey(k => k.Id);
+                m.Property(p => p.Id).HasColumnName("Id");
+                m.Property(p => p.BrandId).HasColumnName("BrandId");
+                m.Property(p => p.Name).HasColumnName("Name");
+                m.Property(p => p.DailyPrice).HasColumnName("DailyPrice");
+                m.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
+                m.HasOne(p => p.Brand);
+            });
+
+            Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
+            modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+
+            Model[] modelEntitySeeds = { new(1, 1, "Series 4", 1500, ""),new(2, 1, "Series 3", 1200, "")
+                    , new(3, 2, "A180", 1000, "")
+                    , new(4, 2, "A190", 900, "") };
+            modelBuilder.Entity<Model>().HasData(modelEntitySeeds);
+
+        }
+    }
+}
